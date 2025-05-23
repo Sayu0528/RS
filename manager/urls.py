6597fdf34196f98553d86998c0d72f9a40796ef0
MASA25025 <genanjiangxi@gmail.com>
2025-05-23 16:11:21 +0900

    仕事内容入力画面作成

--- manager/urls.py
+++ manager/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('shift_schedule/', views.shift_schedule, name='shift_schedule'),
+    path('job_explain/', views.job_explain, name='job_explain'),
 ]
--- manager/views.py
+++ manager/views.py
@@ -4,6 +4,11 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'manager/index.html')
 
+# 出勤者一覧画面
 def shift_schedule(request):
     return render(request, 'manager/shift_schedule.html')
+
+#仕事内容入力画面 
+def job_explain(request):
+    return render(request, 'manager/job_explain.html')
 
