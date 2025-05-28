ad44cda867825e20696f943fd8d14149b1961d72
shikapen <a13763898111@gmail.com>
2025-05-28 13:10:08 +0900

    a

--- manager/urls.py
+++ manager/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     # 管理者用カレンダー一覧
     path('calendar-admin/', views.calendar_admin_view, name='admin_calendar'),
     # 日付クリック後の入力画面（管理者用）
-    path('manager/manage/<date>/', views.input_by_date_admin_view, name='admin_input_by_date'),
+    path('manager/<date>/', views.input_by_date_admin_view, name='admin_input_by_date'),
 ]
